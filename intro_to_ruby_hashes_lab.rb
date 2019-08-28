f9a564ce61160814b757e156f277880c5bedc951
James Batsell <jamesbatsell05@gmail.com>
2019-08-28 05:43:33 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -16,11 +16,12 @@ end
 
 def my_hash_creator(key, value)
  hash = {}
- hash[key] = 
+ hash[key] = value
+ hash
 end
 
 def read_from_hash(hash, key)
-  # return the correct value using the hash and key parameters
+ 
 end
 
 def update_counting_hash(hash, key)
