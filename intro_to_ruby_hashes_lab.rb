504a63898f9c8b868bb16298fec1ca155c886a1c
James Batsell <jamesbatsell05@gmail.com>
2019-08-28 06:06:20 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,10 +27,10 @@ def read_from_hash(hash, key)
 end
 
 def update_counting_hash(hash, key)
-if hash[key]
-    hash[key] += 1 
+if apple[3]
+    apple[3] += 1 
   else
-    hash[key] = 1
+    apple[3] = 1
   end
-  return hash
+  return apple
 end
